d87a693e20f3f6ff189b431830a829b5bbeab27e
Marcus Mazza <m-mazza@live.com>
2025-10-27 13:30:25 -0300

    testando interplação da variáveis

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup>
+    const nome = 'Marcus Mazza';
+    const idade = 38;
 </script>
 
 <template>
-  
+    <h1> O Aluno {{ nome }} tem {{ idade }} anos.</h1>
 </template>
 
 <style scoped>
